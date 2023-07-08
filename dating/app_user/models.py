1e94ac3a345890fd578fb832d653377ca5a4220a
poldy <polyakov.dima.i@yandex.ru>
2023-07-08 15:26:36 +0700

    Добавил модель пользователя

---
+++ dating/app_user/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Поле Email должно быть заполнено')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', "Female")
+    )
+    username = None
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to='avatars/')
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
