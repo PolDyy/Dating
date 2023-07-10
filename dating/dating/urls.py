178309c50df1dac44742e0562e5e3c4d87cf0626
poldy <polyakov.dima.i@yandex.ru>
2023-07-10 21:29:03 +0700

    Настроил проект для деплоя

--- dating/dating/urls.py
+++ dating/dating/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app_user.urls')),
     path('api/', include('app_matches.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
